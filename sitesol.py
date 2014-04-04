4063aad8a991fa8b6aaee6bda29662910587e322
oukiar <oukiar@gmail.com>
2014-04-04 14:19:19 -0600

    Update sitesol.py

--- sitesol.py
+++ sitesol.py
@@ -1,6 +1,23 @@
-
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 
+
+class Sitesol(FloatLAyout):
+    def __init__(self, **kwargs):
+        super(Sitesol, self).__init__(**kwargs)
+        
+        #obtener ip del servidor que resuelve tu ip:puerto de internet
+        
+        
+        self.inet_ip = 
+        self.inet_port = 
+        
+        self.add_widget
+    
+    
+if __name__ == '__main__':
+    
+    
+    
