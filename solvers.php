0babd84f195a3e055d97fb131311ff185774e9fa
oukiar <oukiar@gmail.com>
2014-04-04 15:00:24 -0600

    Update solvers.php

--- solvers.php
+++ solvers.php
@@ -1,6 +1,18 @@
 <?
-//script que almacena y resuelve lalista de servidores netget disponibles
+//script que almacena y resuelve la lista de servidores netget disponibles
 
+if( isset( $_GET['set_server_ip'] ) )
+{
+  $f = fopen('sitesol_server.txt', 'w');
+  fwrite($f, $_SERVER['REMOTE_ADDR']);
+  fclose($f);
+}
+else if( isset( $_GET['get_server_ip']) )
+{
+  $f = fopen('sitesol_server.txt', 'r');
+  $ip = fread($f);
+  echo $ip;
+}
 
 
 ?>
