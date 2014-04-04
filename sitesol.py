cc3a051eb7ffa0720d729cb4b99c35825e815459
oukiar <oukiar@gmail.com>
2014-04-04 17:42:35 -0600

    Update sitesol.py

--- sitesol.py
+++ sitesol.py
@@ -1,4 +1,5 @@
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
@@ -8,9 +9,9 @@ from network import Network
 
 import json, socket, urllib, os
 
-class Sitesol(GridLayout):
+class Sitesol(BoxLayout):
     def __init__(self, **kwargs):
-        super(Sitesol, self).__init__(cols=2, **kwargs)
+        super(Sitesol, self).__init__(**kwargs)
         
         #get network instance from kwargs (arguments by keyword) or create a new network object
         self.net = kwargs.get('net', Network() )
@@ -24,56 +25,59 @@ class Sitesol(GridLayout):
         response = urllib.urlopen('http://www.devsinc.com.mx/netget/solvers.php?get_server_ip=foo')
         self.server_ip = response.read()
         self.server_port = 31415
+        
+        #layout izquierda
+        self.lay_left = GridLayout(cols=2)
                 
         #SERVER
-        self.add_widget(Label(text='Server: ') )
+        self.lay_left.add_widget(Label(text='Server: ') )
         self.txt_serverip = TextInput(text=self.server_ip)
-        self.add_widget(self.txt_serverip)
+        self.lay_left.add_widget(self.txt_serverip)
         
         #WAN IP
         self.lb_wan_ip = Label(text='Wan IP: ')
-        self.add_widget(self.lb_wan_ip)
+        self.lay_left.add_widget(self.lb_wan_ip)
         self.txt_wan_ip = TextInput()
-        self.add_widget(self.txt_wan_ip)
+        self.lay_left.add_widget(self.txt_wan_ip)
         
         #WAN PORT
         self.lb_wan_port= Label(text='Wan Port: ')
-        self.add_widget(self.lb_wan_port)
+        self.lay_left.add_widget(self.lb_wan_port)
         self.txt_wan_port = TextInput()
-        self.add_widget(self.txt_wan_port)
+        self.lay_left.add_widget(self.txt_wan_port)
         
         #separator
-        self.add_widget(ProgressBar())
-        self.add_widget(ProgressBar())
+        self.lay_left.add_widget(ProgressBar())
+        self.lay_left.add_widget(ProgressBar())
         
         
         #REMOTE IP
         self.lb_remote_ip = Label(text='Remote IP: ')
-        self.add_widget(self.lb_remote_ip)
+        self.lay_left.add_widget(self.lb_remote_ip)
         self.txt_remote_ip = TextInput()
-        self.add_widget(self.txt_remote_ip)
+        self.lay_left.add_widget(self.txt_remote_ip)
         
         #REMOTE PORT
         self.lb_remote_port= Label(text='Remote Port: ')
-        self.add_widget(self.lb_remote_port)
+        self.lay_left.add_widget(self.lb_remote_port)
         self.txt_remote_port = TextInput()
-        self.add_widget(self.txt_remote_port)
+        self.lay_left.add_widget(self.txt_remote_port)
         
 
         #separator
-        self.add_widget(ProgressBar())
-        self.add_widget(ProgressBar())
+        self.lay_left.add_widget(ProgressBar())
+        self.lay_left.add_widget(ProgressBar())
         
         
         #nickname
-        self.add_widget(Label(text='Nick: ') )
+        self.lay_left.add_widget(Label(text='Nick: ') )
         self.txt_nick = TextInput(text='your nickname')
-        self.add_widget(self.txt_nick)
+        self.lay_left.add_widget(self.txt_nick)
         
         #mensaje
-        self.add_widget(Label(text='Mensaje: ') )
+        self.lay_left.add_widget(Label(text='Mensaje: ') )
         self.txt_msg = TextInput()
-        self.add_widget(self.txt_msg)
+        self.lay_left.add_widget(self.txt_msg)
         
         
         
@@ -81,13 +85,16 @@ class Sitesol(GridLayout):
         self.btn_solveudp = Button(text='Resolver IP', 
                                     on_press=self.on_solveudp)
                                     
-        self.add_widget(self.btn_solveudp)
+        self.lay_left.add_widget(self.btn_solveudp)
         
         
         self.btn_sendmsg = Button(text='Enviar', 
                                     on_press=self.on_sendmsg)
                                     
-        self.add_widget(self.btn_sendmsg)
+        self.lay_left.add_widget(self.btn_sendmsg)
+        
+        #layout izquierdo
+        self.add_widget(self.lay_left)
         
         #history
         self.txt_messages = TextInput(text='Mensajes:')
@@ -100,6 +107,11 @@ class Sitesol(GridLayout):
     def on_sendmsg(self, w):
         tosend = json.dumps({'msg':'message', 'data':self.txt_msg.text})
         self.net.send((self.txt_remote_ip.text, int(self.txt_remote_port.text)), tosend)
+        
+        
+        self.txt_messages.text += 'Enviado:\n' + self.txt_msg.text
+        self.txt_msg.text = ''
+        
          
     def on_solveudp(self, w):
         tosend = json.dumps({'msg':'get_inet_addr', 'data':None})
@@ -124,7 +136,7 @@ class Sitesol(GridLayout):
             self.txt_wan_port.text = str(port)
             
         elif data_dict['msg'] == 'message':
-            self.txt_messages.text += '\n' + data
+            self.txt_messages.text += 'Recibido:\n' + data
             
             
                
