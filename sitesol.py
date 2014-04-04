37aadfc9c68f297f1c49d4a548f3b10a1f015ba8
oukiar <oukiar@gmail.com>
2014-04-04 14:29:39 -0600

    Update sitesol.py

--- sitesol.py
+++ sitesol.py
@@ -8,9 +8,20 @@ class Sitesol(FloatLAyout):
     def __init__(self, **kwargs):
         super(Sitesol, self).__init__(**kwargs)
         
+        #get network instance from kwargs (arguments by keyword) or create a new network object
+        self.net = kwargs.get('net', Network() )
+        
+        #exist connection?
+        if not self.net.has_connection():
+            #create a new connection
+            self.net.create_connection(self.receiver)
+        
         #obtener ip del servidor que resuelve tu ip:puerto de internet
+        r = urllib.urlopen('http://www.devsinc.com.mx/netget/solvers.php?solveip=foo')
         
+        self.server = 
         
+        #guardar nuestra informacion de conexion respecto a internet
         self.inet_ip = 
         self.inet_port = 
         
