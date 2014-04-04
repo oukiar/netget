12bd9986998a7c6dc51c6c6c81f4fadfa7674255
oukiar <oukiar@gmail.com>
2014-04-04 16:09:35 -0600

    Update sitesol.py

--- sitesol.py
+++ sitesol.py
@@ -3,8 +3,11 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 
+from network import Network
 
-class Sitesol(FloatLAyout):
+import json, socket, urllib, os
+
+class Sitesol(FloatLayout):
     def __init__(self, **kwargs):
         super(Sitesol, self).__init__(**kwargs)
         
@@ -16,18 +19,49 @@ class Sitesol(FloatLAyout):
             #create a new connection
             self.net.create_connection(self.receiver)
         
-        #obtener ip del servidor que resuelve tu ip:puerto de internet
-        r = urllib.urlopen('http://www.devsinc.com.mx/netget/solvers.php?get_server_ip=foo')
-        self.server = self.server_ip = r.read()
+        #obtener ip del servidor que resuelve tu ip:puerto UDP de internet
+        response = urllib.urlopen('http://www.devsinc.com.mx/netget/solvers.php?get_server_ip=foo')
+        self.server_ip = response.read()
+        self.server_port = 31415
         
-        #guardar nuestra informacion de conexion respecto a internet
-        self.inet_ip = 
-        self.inet_port = 
+        #guardar nuestra informacion de conexion respecto a internet (WAN connection)
+        self.inet_ip = None
+        self.inet_port = None
         
-        self.add_widget
-    
-    
+        self.add_widget(Label(text='Server: '+self.server_ip) )
+        
+        self.btn_solveudp = Button(text='Get My IP', 
+                                    on_press=self.on_solveudp, 
+                                    size_hint=(None,None))
+                                    
+        self.add_widget(self.btn_solveudp)
+        
+    def on_solveudp(self, w):
+        
+        tosend = json.dumps({'msg':'get_inet_addr', 'data':None})
+        self.net.send((self.server_ip, self.server_port), tosend)
+         
+    def receiver(self, data, addr):
+        #unpack
+        data_dict = json.loads(data)
+
+        #extract data
+        data = data_dict['data']
+        
+        #analize
+        if data_dict['msg'] == 'ping':
+            tosend = json.dumps({'msg':'ping_ack', 'data':socket.gethostname()})
+            self.net.send(addr, tosend)
+        elif data_dict['msg'] == 'addr_udp':
+            print data
+            
+               
 if __name__ == '__main__':
     
+    from kivy.base import runTouchApp
+    
+    client = Sitesol()
+    runTouchApp(client)
+    client.net.shutdown_network()
     
     
