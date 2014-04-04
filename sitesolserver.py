e1dfb541bc4ac657a6add64f59714c7f3ff547d3
oukiar <oukiar@gmail.com>
2014-04-04 15:16:07 -0600

    Update sitesolserver.py

--- sitesolserver.py
+++ sitesolserver.py
@@ -13,11 +13,37 @@ import json, socket, urllib, os
 class SitesolServer(FloatLayout):
     def __init__(self, **kwargs):
         super(SitesolServer, self).__init__(**kwargs)
+        
+        #get network instance from kwargs (arguments by keyword) or create a new network object
+        self.net = kwargs.get('net', Network() )
+        
+        #exist connection?
+        if not self.net.has_connection():
+            #create a new connection
+            self.net.create_connection(self.receiver)
 
         #screen logger
         self.txt_log = TextInput(text='Netget server initiated')
         self.add_widget(self.txt_log)
 
         #actualizar en el servidor esta IP    
-        r = urllib.urlopen('http://www.devsinc.com.mx/netget/server_ip.php?update_server_ip=foo')
+        r = urllib.urlopen('http://www.devsinc.com.mx/netget/solvers.php?set_server_ip=foo')
          
+    def receiver(self, data, addr):
+        #unpack
+        data_dict = json.loads(data)
+        
+        self.txt_log.text += '\nMessage: ' + data_dict['msg']
+        
+        
+        data = data_dict['data']
+        
+        #analize
+        if data_dict['msg'] == 'ping':
+            tosend = json.dumps({'msg':'ping_ack', 'data':socket.gethostname()})
+            self.net.send(addr, tosend)
+        elif data_dict['msg'] == 'get_inet_addr':
+            pass
+            
+            
+            
