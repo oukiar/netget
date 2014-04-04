aed0bf73531ed87d892af2666ab0e14721d6fc0b
oukiar <oukiar@gmail.com>
2014-04-04 17:26:32 -0600

    Update sitesol.py

--- sitesol.py
+++ sitesol.py
@@ -1,15 +1,16 @@
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
+from kivy.uix.progressbar import ProgressBar
 
 from network import Network
 
 import json, socket, urllib, os
 
-class Sitesol(FloatLayout):
+class Sitesol(GridLayout):
     def __init__(self, **kwargs):
-        super(Sitesol, self).__init__(**kwargs)
+        super(Sitesol, self).__init__(cols=2, **kwargs)
         
         #get network instance from kwargs (arguments by keyword) or create a new network object
         self.net = kwargs.get('net', Network() )
@@ -23,21 +24,84 @@ class Sitesol(FloatLayout):
         response = urllib.urlopen('http://www.devsinc.com.mx/netget/solvers.php?get_server_ip=foo')
         self.server_ip = response.read()
         self.server_port = 31415
+                
+        #SERVER
+        self.add_widget(Label(text='Server: ') )
+        self.txt_serverip = TextInput(text=self.server_ip)
+        self.add_widget(self.txt_serverip)
         
-        #guardar nuestra informacion de conexion respecto a internet (WAN connection)
-        self.inet_ip = None
-        self.inet_port = None
+        #WAN IP
+        self.lb_wan_ip = Label(text='Wan IP: ')
+        self.add_widget(self.lb_wan_ip)
+        self.txt_wan_ip = TextInput()
+        self.add_widget(self.txt_wan_ip)
         
-        self.add_widget(Label(text='Server: '+self.server_ip) )
+        #WAN PORT
+        self.lb_wan_port= Label(text='Wan Port: ')
+        self.add_widget(self.lb_wan_port)
+        self.txt_wan_port = TextInput()
+        self.add_widget(self.txt_wan_port)
         
-        self.btn_solveudp = Button(text='Get My IP', 
-                                    on_press=self.on_solveudp, 
-                                    size_hint=(None,None))
+        #separator
+        self.add_widget(ProgressBar())
+        self.add_widget(ProgressBar())
+        
+        
+        #REMOTE IP
+        self.lb_remote_ip = Label(text='Remote IP: ')
+        self.add_widget(self.lb_remote_ip)
+        self.txt_remote_ip = TextInput()
+        self.add_widget(self.txt_remote_ip)
+        
+        #REMOTE PORT
+        self.lb_remote_port= Label(text='Remote Port: ')
+        self.add_widget(self.lb_remote_port)
+        self.txt_remote_port = TextInput()
+        self.add_widget(self.txt_remote_port)
+        
+
+        #separator
+        self.add_widget(ProgressBar())
+        self.add_widget(ProgressBar())
+        
+        
+        #nickname
+        self.add_widget(Label(text='Nick: ') )
+        self.txt_nick = TextInput(text='your nickname')
+        self.add_widget(self.txt_nick)
+        
+        #mensaje
+        self.add_widget(Label(text='Mensaje: ') )
+        self.txt_msg = TextInput()
+        self.add_widget(self.txt_msg)
+        
+        
+        
+        #buttons
+        self.btn_solveudp = Button(text='Resolver IP', 
+                                    on_press=self.on_solveudp)
                                     
         self.add_widget(self.btn_solveudp)
         
-    def on_solveudp(self, w):
         
+        self.btn_sendmsg = Button(text='Enviar', 
+                                    on_press=self.on_sendmsg)
+                                    
+        self.add_widget(self.btn_sendmsg)
+        
+        #history
+        self.txt_messages = TextInput(text='Mensajes:')
+        self.add_widget(self.txt_messages)
+        
+        
+        #try to solve my udp info
+        self.on_solveudp(None)
+        
+    def on_sendmsg(self, w):
+        tosend = json.dumps({'msg':'message', 'data':self.txt_msg.text})
+        self.net.send((self.txt_remote_ip.text, int(self.txt_remote_port.text)), tosend)
+         
+    def on_solveudp(self, w):
         tosend = json.dumps({'msg':'get_inet_addr', 'data':None})
         self.net.send((self.server_ip, self.server_port), tosend)
          
@@ -53,7 +117,15 @@ class Sitesol(FloatLayout):
             tosend = json.dumps({'msg':'ping_ack', 'data':socket.gethostname()})
             self.net.send(addr, tosend)
         elif data_dict['msg'] == 'addr_udp':
-            print data
+            
+            ip, port = json.loads(data)
+            
+            self.txt_wan_ip.text = ip
+            self.txt_wan_port.text = str(port)
+            
+        elif data_dict['msg'] == 'message':
+            self.txt_messages.text += '\n' + data
+            
             
                
 if __name__ == '__main__':
